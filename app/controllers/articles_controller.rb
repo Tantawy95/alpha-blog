cebcc5dbf8ff1e6ffe57237d1f9929fbee1ab3ee
Ali Tantawy <ali.e.tantawy@gmail.com>
2021-02-15 15:23:32 +0200

    DRY code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,35 +1,36 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
+
   def index
     @articles = Article.all
   end
+
   def new
     @article = Article.new
   end
+
   def edit
   
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    
+    @article = Article.new(params_whitelisting)
     if @article.save
       flash[:notice] = "Your article was saved successfully"
     redirect_to @article
     else
       render 'new'
     end
-    
-    
-
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    #@article = Article.find(params[:id])
+    if @article.update(params_whitelisting)
         flash[:notice] = "Article is updated successfully"
         redirect_to @article
     else
@@ -37,9 +38,17 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
     @article.destroy
     redirect_to @article 
+  end
+
+  private 
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def params_whitelisting
+    params.require(:article).permit(:title, :description)
   end
  
 end
