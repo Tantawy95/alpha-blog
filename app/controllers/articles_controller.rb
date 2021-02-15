55cc663a9eb8de53cf5a42ad9139ff202cea7ff6
Ali Tantawy <ali.e.tantawy@gmail.com>
2021-02-15 14:05:54 +0200

    Delete an article and adding show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,11 @@ class ArticlesController < ApplicationController
     else
         render 'edit'
     end
-    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article 
+  end
  
 end
